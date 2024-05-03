c77114bd95c7dae5d85f9b83c158cc31f8af00b3
50chin <50chin50@gmail.com>
2024-05-03 15:18:09 +0300

    added home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
-
-import { MobileApp } from "../../components/MobileApp/MobileApp";
-import { Statistics } from "../../components/Statistics/Statistics";
-import s from "./Home.module.scss";
-import { Layout } from "../../layouts/Layout/Layout";
+import { MobileApp } from '../../components/MobileApp/MobileApp';
+import { Statistics } from '../../components/Statistics/Statistics';
+import s from './Home.module.scss';
+import { Layout } from '../../layouts/Layout/Layout';
+import { Hero } from '../../components/Hero/Hero';
 
 export const Home = () => {
   return (
-    <div>
-      <Statistics></Statistics>
-      <MobileApp></MobileApp>
-    </div>
+    <>
+      <Layout>
+        <Hero></Hero>
+        <Statistics></Statistics>
+        <MobileApp></MobileApp>
+      </Layout>
+    </>
   );
 };
