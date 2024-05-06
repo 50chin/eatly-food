e52c07e9f8ffc240263ea0a93e57042b2b248427
Anthony <programmistanton@gmail.com>
2024-05-06 19:07:10 +0300

    Add logic for commponent and fix style for component Tag

--- src/components/ArticleDishes/ArticleDishes.jsx
+++ src/components/ArticleDishes/ArticleDishes.jsx
@@ -1,25 +1,52 @@
+import React, { useState } from 'react';
 import s from "./ArticleDishes.module.scss";
 import img from './img/FoodImage.png';
 import heart from './img/Heart.svg';
 import star from './img/Star.svg';
+import { Tag } from "../Tag/Tag";
 
-export const ArticleDishes = () => {
-  return <article className={s.card}>
-    <div className={s.card__content}>
+export const ArticleDishes = ({type, image, names, duration, rating, dollars, cents}) => {
+  const [count, setCount] = useState(0); 
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount(prevCount => prevCount > 1 ? prevCount - 1 : 0);
+  };
+
+  const firstBig = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+  return (
+    <article className={s.card}>
+      <div className={s.card__content}>
         <div className={s.card__images}>
-            <img src={img} alt="img" className={s.card__img}/>
+            <img src={image} alt="img" className={s.card__img}/>
             <input type="image" src={heart} className={s.card__heart}/>
         </div>
-        <p className={s.card__title}>Chicken Hell</p>
+        <Tag>{firstBig(type)}</Tag>
+        <p className={s.card__title}>{names}</p>
         <div className={s.card__estimation}>
-            <p className={s.card__p}>24min •</p>
+            <p className={s.card__p}>{duration}min •</p>
             <img src={star} alt="img" className={s.card__star}/>
-            <p className={s.card__p}>4.8</p>
+            <p className={s.card__p}>{rating}</p>
         </div>
         <div className={s.card__price}>
-            <p className={s.card__p}><span className={s.card__ceil}>$12</span>.99</p>
-            <button className={s.card__btn}>+</button>
+            <p className={s.card__p}><span className={s.card__ceil}>${dollars}</span>.{cents}</p>
+            {count === 0 ? (
+              <button className={s.card__btn} onClick={increment}>+</button>
+            ) : (
+              <div>
+                <button className={`${s.card__btn} ${s.card__btn_decr}`} onClick={decrement}>-</button>
+                <span className={s.card__count}>{count}</span>
+                <button className={s.card__btn} onClick={increment}>+</button>
+              </div>
+            )}
         </div>
-    </div>
-  </article>;
+      </div>
+    </article>
+  );
 };
