4a44f1c14e9d6a60c3bffba86cadfe51793aec47
50chin <50chin50@gmail.com>
2024-05-06 18:55:00 +0300

    added favorites and bookMark buttons

--- src/components/ArticleDishes/ArticleDishes.jsx
+++ src/components/ArticleDishes/ArticleDishes.jsx
@@ -1,25 +1,31 @@
-import s from "./ArticleDishes.module.scss";
+import { AddButton } from '../../ui/AddButton/AddButton';
+import { FavoritesButton } from '../../ui/FavoritesButton';
+import s from './ArticleDishes.module.scss';
 import img from './img/FoodImage.png';
 import heart from './img/Heart.svg';
 import star from './img/Star.svg';
 
 export const ArticleDishes = () => {
-  return <article className={s.card}>
-    <div className={s.card__content}>
+  return (
+    <article className={s.card}>
+      <div className={s.card__content}>
         <div className={s.card__images}>
-            <img src={img} alt="img" className={s.card__img}/>
-            <input type="image" src={heart} className={s.card__heart}/>
+          <img src={img} alt="img" className={s.card__img} />
+          <FavoritesButton />
         </div>
         <p className={s.card__title}>Chicken Hell</p>
         <div className={s.card__estimation}>
-            <p className={s.card__p}>24min •</p>
-            <img src={star} alt="img" className={s.card__star}/>
-            <p className={s.card__p}>4.8</p>
+          <p className={s.card__p}>24min •</p>
+          <img src={star} alt="img" className={s.card__star} />
+          <p className={s.card__p}>4.8</p>
         </div>
         <div className={s.card__price}>
-            <p className={s.card__p}><span className={s.card__ceil}>$12</span>.99</p>
-            <button className={s.card__btn}>+</button>
+          <p className={s.card__p}>
+            <span className={s.card__ceil}>$12</span>.99
+          </p>
+          <AddButton />
         </div>
-    </div>
-  </article>;
+      </div>
+    </article>
+  );
 };
---
+++ src/ui/AddButton/AddButton.jsx
@@ -0,0 +1,5 @@
+import s from './AddButton.module.scss';
+
+export const AddButton = () => {
+  return <button className={s.btn}>+</button>;
+};
