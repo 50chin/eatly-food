91854359bd68e2792d48e257c4363c070545d286
westheimer77 <cherrypilot7@gmail.com>
2024-05-08 22:48:09 +0300

    feat: fix slider

--- src/components/MenuHeroSection/MenuHeroSection.jsx
+++ src/components/MenuHeroSection/MenuHeroSection.jsx
@@ -4,57 +4,36 @@ import s from "./MenuHeroSection.module.scss";
 import img from "../../assets/img/Menu/banner.png";
 import img2 from "../../assets/img/Menu/banner2.webp";
 import img3 from "../../assets/img/Menu/banner3.jpg";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 
-
-
 export const MenuHeroSection = () => {
-    
-    const button1 = document.getElementById(0)
-    const button2 = document.getElementById(1);
-    const button3 = document.getElementById(2);
-
-
     const sliderBase = [img, img2, img3];
     const [slide, setSlide] = useState(sliderBase[0]);
     const [classButton, setClassButton] = useState(0);
-    const button1Id = 0;
-    const button2Id = 1;
-    const button3Id = 2;
 
-    function swapSlide() {
+    const swapSlide = () => {
         let index = sliderBase.indexOf(slide);
         if (index == 2) {
             index = 0;
             setSlide(sliderBase[index]);
-            setClassButton(index)
+            setClassButton(index);
         } else {
             index += 1;
             setSlide(sliderBase[index]);
-            setClassButton(index)
+            setClassButton(index);
         }
-    }
+    };
 
     useEffect(() => {
         const slideTimer = setInterval(swapSlide, 3000);
         return () => clearInterval(slideTimer);
     }, [slide]);
 
-    useEffect(() => {
-        buttonActive
-    }, [slide])
-    
     const buttonActive = (evt) => {
-        setClassButton(evt.target.id);
-        console.log(classButton);
-        setSlide(sliderBase[evt.target.id])
-        evt.target.classList.replace(
-            s.menu__sliderLine,
-            s.menu__sliderLine_purple
-        );
-    }
+        setSlide(sliderBase[evt.target.id]);
+        setClassButton(Number(evt.target.id));
+    };
 
     return (
         <section className={s.menu}>
@@ -64,9 +43,19 @@ export const MenuHeroSection = () => {
                         <div className={s.menu__slider}>
                             <img className={s.menu__image} src={slide} alt="" />
                             <div className={s.menu__sliderLines}>
-                                <button  onClick={buttonActive} id={button1Id} className={clsx(s.button, {[s.menu__sliderLine_purple]: classButton == button1Id, [s.menu__sliderLine]: button1Id != classButton})}></button>
-                                <button onClick={buttonActive} id={button2Id} className={clsx(s.button, {[s.menu__sliderLine_purple]: classButton == button2Id, [s.menu__sliderLine]: button2Id != classButton})}></button>
-                                <button onClick={buttonActive} id={button3Id} className={clsx(s.button, {[s.menu__sliderLine_purple]: classButton == button3Id, [s.menu__sliderLine]: button3Id != classButton})}></button>
+                                {[...Array(3).keys()].map((i) => (
+                                    <button
+                                        key={i}
+                                        onClick={buttonActive}
+                                        id={i}
+                                        className={clsx(s.button, {
+                                            [s.menu__sliderLine_purple]:
+                                                classButton == i,
+                                            [s.menu__sliderLine]:
+                                                classButton != i,
+                                        })}
+                                    ></button>
+                                ))}
                             </div>
                         </div>
                         <div className={s.menu__search}>
